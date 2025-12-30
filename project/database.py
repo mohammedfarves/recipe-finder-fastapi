bb861c69a4b2cbcd5fe4d924460bf29a8c2dc8c6
MOHAMMED FARVES <142089394+mohammedfarves@users.noreply.github.com>
2025-12-30 10:34:59 +0530

    Update database.py

--- project/database.py
+++ project/database.py
@@ -1,11 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/fastapi_todo"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable not set")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
 
 def get_db():
